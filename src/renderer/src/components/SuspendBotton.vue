67d239c0e655446f5bff663b3525f77f063f151d
huangshihao <84605859@qq.com>
2023-08-10 22:14:08 +0800

    悬浮窗

---
+++ src/renderer/src/components/SuspendBotton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div @click="emits('modelExpand')"> {{ props.modelExpand ? '111': '222' }} </div>
+  <div @click="emits('modelPreview')"> {{ props.modelPreview ? '333': '444' }} </div>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    modelPreview: boolean
+    modelExpand: boolean
+  }>(),
+  {
+    modelPreview: false,
+    modelExpand: false
+  }
+)
+
+const emits = defineEmits<{
+  (e: 'modelPreview') :void
+  (e: 'modelExpand') :void
+}>()
+</script>
